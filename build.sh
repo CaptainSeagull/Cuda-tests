be1f1588f493f2a1f37632fba3ff7ef8eee217f5
Jonathan Livingstone <seagull127@ymail.com>
2017-01-14 21:37:53 +0000

    dsfjkl

--- build.sh
+++ build.sh
@@ -1,14 +1,16 @@
 # @Author: jonathan
 # @Date:   2017-01-13 17:03:42
-# @Last Modified by:   jonathan
-# @Last Modified time: 2017-01-13 17:15:23
+# @Last Modified by:   Jonathan Livingstone
+# @Last Modified time: 2017-01-14 19:06:31
 
 RELEASE=false
 
+VERSION=1
+
 pushd build
 if [ "$RELEASE" = "true" ]; then
-    nvcc -o "Demo0" ../code/demo0.cu
+    nvcc -o "Demo""$VERSION" "../code/demo""$VERSION"".cu"
 else
-    nvcc -o "Demo0" -g -G ../code/demo0.cu
+    nvcc -o "Demo""$VERSION" -g -G "../code/demo""$VERSION"".cu"
 fi
 popd
